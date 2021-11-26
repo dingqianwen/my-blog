9cf1de4341033625766b3bc13ead9672ce615cec
丁乾文 <761945125@qq.com>
2021-11-26 18:27:32 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: "My-Blog",
+    base: "/blog/",
+    // /blog/  nginx配置为
+    // location /blog {
+    // alias  html/blog/;
+    // index index.html;
+    // }
+    description: '这是丁乾文的博客🐮👃!',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: "logo.png"
+        }]
+    ],
+    dest: './docs/.vuepress/blog',
+    evergreen: true,
+    themeConfig: {
+        logo: 'logo.png',  //网页顶端导航栏左上角的图标
+        navbar: [
+            {text: 'Home', link: '/'},
+            {text: '指南', link: '/quickstart/'},
+            {text: '简介', link: '/bi'},
+            {text: 'Gitee', link: 'https://gitee.com/qwding'},
+            {text: 'Github', link: 'https://github.com/dingqianwen'},
+        ],
+        sidebarDepth: 2,
+        sidebar: {
+            '/quickstart/': [
+                {
+                    text: '快速入门',
+                    children: ['/quickstart/README.md'],
+                },
+            ],
+            '/java/': [
+                {
+                    text: 'Java',
+                    children: ['/java/README.md', '/java/架构师成长之路.md'],
+                },
+            ],
+        }
+    },
+};
+
---
+++ docs/README.md
@@ -0,0 +1,33 @@
+---
+home: true
+heroImage: logo.png
+
+actions:
+- text: 快速上手
+  link: /quickstart/
+  type: primary
+- text: 项目简介
+  link: /guide/
+  type: secondary
+
+features:
+- title: 牛逼
+  details: 🐮👃。
+- title: 无敌
+  details: 无敌是多么寂寞。
+- title: 寂寞
+  details: 找不到对手而忧伤。
+  footer: Apache-2.0 Licensed | Copyright © 2020
+---
+
+
+###  像数 1, 2, 3 一样容易
+```java
+
+    public static void main(String[] args) {
+        System.out.println |
+    }
+    
+```
+123
+```
