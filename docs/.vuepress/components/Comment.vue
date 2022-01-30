4fcd8c21d7f13327617dc0708119f79d9f13b1ed
丁乾文 <761945125@qq.com>
2022-01-30 10:13:05 +0800

    修复歌词滚动问题

--- docs/.vuepress/components/Comment.vue
+++ docs/.vuepress/components/Comment.vue
@@ -14,8 +14,8 @@ export default {
   mounted() {
     // alert(decodeURI(location.pathname))
     const commentConfig = {
-      clientID: '',
-      clientSecret: '',
+      clientID: '5fed2d25f64a11c27c1b',
+      clientSecret: 'd834f6f2dc273b8dd2aca71ebc159e0036b5c5ca',
       repo: 'my-blog', // 仓库地址
       owner: 'dingqianwen',
       admin: ['dingqianwen'],
