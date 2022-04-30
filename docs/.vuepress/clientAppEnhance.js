1ad40b343aa21027407f2838bddc31493fdb426f
丁乾文 <761945125@qq.com>
2022-04-30 12:48:51 +0800

    bug修复

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -1,24 +1,9 @@
-import {defineClientAppEnhance, usePageData} from '@vuepress/client'
-import md5 from "blueimp-md5";
-import {pvIncr} from './components/api'
+import {defineClientAppEnhance, usePageData} from '@vuepress/client';
 
 export default defineClientAppEnhance(({app, router, siteData}) => {
-    try {
-        // 解决当项目构建时，会触发走此处逻辑， 方案就是在构建时document 是不存在的，抛出异常
-        document.location;
-
-        router.beforeEach((to, from, next) => {
-            next();
-        })
-        router.afterEach((to, from, next) => {
-            // pv ++
-            let value = usePageData().value;
-            let id = md5(value.path);
-            pvIncr(id, function (data) {
-                // console.log(`当前页面:${value.title},访问量:${data}`)
-            })
-        })
-    } catch (e) {
-        // 忽略
-    }
+    router.beforeEach((to, from, next) => {
+        next();
+    })
+    router.afterEach((to, from, next) => {
+    })
 })
