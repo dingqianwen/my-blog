0c24e2d7ef4548381dd11ab22b799edbe227e4df
qwding <761945125@qq.com>
2022-05-02 22:05:49 +0800

    AdsbyGoogle.vue

--- docs/.vuepress/components/AdsbyGoogle.vue
+++ docs/.vuepress/components/AdsbyGoogle.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="adsbygoogle">
+  <div v-if="view">
     <br>
-    <div id="abgby">
-    </div>
+    <ins class="adsbygoogle"
+         v-if="layout==='in-article'"
+         style="display:block; text-align:center;width: 100%"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         :data-ad-client="client"
+         :data-ad-slot="slot"></ins>
+    <ins class="adsbygoogle"
+         v-else
+         style="display:block;width: 100%"
+         :data-ad-client="client"
+         :data-ad-slot="slot"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <br>
   </div>
 </template>
 
 
 <script>
-
-import {ElementResize} from './utils/ElementResize.js';
-
 export default {
   name: "AdsbyGoogle.vue",
   props: ["layout", "slot"],
   data() {
     return {
       client: 'ca-pub-6495628091556233',
-      originWidth: window.innerWidth,
-      screenWidth: window.innerWidth,
+      view: true,
     }
   },
-  methods: {
-    adsbyGooglePush() {
+  mounted() {
+    if (typeof window !== 'undefined') {
       this.$nextTick(() => {
         try {
           // <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6495628091556233" crossorigin="anonymous"/>
@@ -31,67 +40,6 @@ export default {
           console.log("adsbygoogle error ：", e)
         }
       });
-    },
-    setIns() {
-      let element = document.getElementById('abgby');
-      if (this.layout === 'in-article') {
-        element.innerHTML = `
-            <ins class="adsbygoogle"
-         style="display:block; text-align:center;"
-         data-ad-layout="in-article"
-         data-ad-format="fluid"
-         data-ad-client="${this.client}"
-         data-ad-slot="${this.slot}"></ins>
-        `;
-      } else {
-        element.innerHTML = `
-              <ins class="adsbygoogle"
-           style="display:block;"
-           data-ad-client="${this.client}"
-           data-ad-slot="${this.slot}"
-           data-ad-format="auto"
-           data-full-width-responsive="true"></ins>
-        `;
-      }
-    }
-  },
-  mounted() {
-    if (typeof window !== 'undefined') {
-      // init
-      this.setIns();
-      this.adsbyGooglePush();
-
-      // resize
-      const debounce = (fn, delay) => {
-        let timer;
-        return function () {
-          if (timer) {
-            clearTimeout(timer);
-          }
-          timer = setTimeout(() => {
-            fn();
-          }, delay);
-        }
-      };
-      const refresh = () => {
-        this.screenWidth = window.innerWidth;
-      };
-      // 500
-      const cancalDebounce = debounce(refresh, 600);
-      window.addEventListener('resize', cancalDebounce);
-    }
-  }, watch: {
-    screenWidth: {
-      immediate: true,
-      handler(newValue) {
-        if (newValue !== this.originWidth) {
-          console.log("===>" + newValue)
-          // 重新set
-          this.setIns();
-          console.log("refresh")
-          this.adsbyGooglePush();
-        }
-      }
     }
   }
 }
@@ -99,3 +47,4 @@ export default {
 
 <style scoped>
 </style>
+
