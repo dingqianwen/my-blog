2aaf3183a3051c03317429ced843b630f1d5fbda
qwding <761945125@qq.com>
2022-05-18 20:21:50 +0800

    delete clientAppEnhance.js

--- docs/.vuepress/clientAppEnhance.js
+++
@@ -1,9 +0,0 @@
-import {defineClientAppEnhance, usePageData} from '@vuepress/client';
-
-export default defineClientAppEnhance(({app, router, siteData}) => {
-    router.beforeEach((to, from, next) => {
-        next();
-    })
-    router.afterEach((to, from, next) => {
-    })
-})
