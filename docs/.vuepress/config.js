4f69b014173df3674e87150c555bdec32008f2fb
qwding <761945125@qq.com>
2022-05-20 03:06:58 +0800

    bug修复

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,7 +35,7 @@ module.exports = {
         ['meta', {name: 'theme-color', content: '#3eaf7c'}],
         ['link', {
             rel: 'icon',
-            href: "logo.png"
+            href: "/logo.png"
         }],
         [
             "script",
