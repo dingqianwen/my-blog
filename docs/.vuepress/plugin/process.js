c9232b4ac44ada8fc3e0f584aa56e91771def5b9
qwding <761945125@qq.com>
2022-06-02 13:13:52 +0800

    代码优化

--- docs/.vuepress/plugin/process.js
+++ docs/.vuepress/plugin/process.js
@@ -2,23 +2,24 @@ const fs = require('fs')
 const rootDir = './docs'
 const tag = "type: cds";
 const timelineFilePath = "./docs/Timeline.md";
-// 时间线数据
-const timelineObjs = [];
+const pageData = [];
 
-function Content(title, path, createTime) {
+function Content(title, path, fileName, dirFilePath, createTime) {
     this.title = title
     this.path = path
+    this.dirFilePath = dirFilePath
+    this.fileName = fileName
     this.createTime = createTime
 }
 
-function generateDirectory(dir, callback) {
+function listDirectory(dir, callback) {
     let files = fs.readdirSync(dir);
     files.forEach((fileName) => {
         let file = `${dir}/${fileName}`
         if (fileName.indexOf('.') !== 0) {
             let stat = fs.statSync(file);
             if (stat.isDirectory()) {
-                generateDirectory(file, callback)
+                listDirectory(file, callback)
             } else {
                 callback(dir, file)
             }
@@ -26,7 +27,7 @@ function generateDirectory(dir, callback) {
     })
 }
 
-function writeDirectoryData(dir, file) {
+function writePageData(dir, file) {
     if (!file.endsWith("README.md")) {
         return;
     }
@@ -34,7 +35,6 @@ function writeDirectoryData(dir, file) {
     if (content.indexOf(tag) === -1) {
         return;
     }
-    let newTag = '';
     let data = fs.readdirSync(dir);
     for (let i = 0; i < data.length; i++) {
         let f = data[i];
@@ -59,35 +59,52 @@ function writeDirectoryData(dir, file) {
                 title = title.substr(0, title.length - 1);
             }
         }
-
-        /*
-             开始生成时间线
-             <p>
-             statSync return:
-             atime: 2022-01-30T17:37:19.610Z,
-             mtime: 2022-01-30T17:36:46.718Z,
-             ctime: 2022-01-30T17:36:46.718Z,
-             birthtime: 2022-01-30T17:35:00.330Z  注意这里有部分系统不支持birthtime
-             ...
-         */
+        // 注意这里有部分系统不支持birthtime
         let {birthtime} = fs.statSync(dir + "/" + f);
-        let content = new Content(title, dir.replace(rootDir, '') + "/" + f, birthtime);
-        timelineObjs.push(content)
-        // 生成当前目录
-        newTag += `- [${title}](${f})  \n`;
+        let content = new Content(title, dir.replace(rootDir, '') + "/" + f, f, file, birthtime);
+        pageData.push(content)
     }
-    // newTag === '' ?? 内容如何 ？
-    if (newTag === "") {
-        newTag = '> 暂无内容 \n';
-    }
-
-    let newContent = content.replace(/(?<=\[dir.start]: <>)[^]*?(?=\[dir.end]: <>)/, "\n\n" + newTag + "\n");
-    fs.writeFileSync(file, newContent);
 }
 
+function generateDirectory() {
+    let pageGroup = pageData.reduce((acc, item) => {
+        if (acc[item.dirFilePath]) {
+            acc[item.dirFilePath].push(item);
+        } else {
+            acc[item.dirFilePath] = [item];
+        }
+        return acc;
+    }, {});
+    Object.keys(pageGroup).forEach(dirFilePath => {
+        let element = pageGroup[dirFilePath];
+        let newTag = '';
+        for (let pd of element) {
+            // 生成当前目录
+            newTag += `- [${pd.title}](${pd.fileName})  \n`;
+        }
+        // newTag === '' ?? 内容如何 ？
+        if (newTag === "") {
+            newTag = '> 暂无内容 \n';
+        }
+        // 写入目录
+        fs.readFile(dirFilePath, function (err, content) {
+            let newContent = content.toString().replace(/(?<=\[dir.start]: <>)[^]*?(?=\[dir.end]: <>)/, "\n\n" + newTag + "\n");
+            fs.writeFile(dirFilePath, newContent, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+    })
+}
 
-function generateTimeline(file, obj) {
+function generateTimeline(file) {
     let map = {}
+    let obj = Array.from(pageData).map(m => ({
+        title: m.title,
+        path: m.path,
+        createTime: m.createTime
+    }))
     for (let i = 0; i < obj.length; i++) {
         const to = obj[i];
         let fullYear = to.createTime.getFullYear();
@@ -115,17 +132,23 @@ function generateTimeline(file, obj) {
         }
     }
 
-    let content = fs.readFileSync(file).toString();
-    let data = `\n maps: ${JSON.stringify(map, null, 3)} \n`;
-    let newContent = content.replace(/(?<=\/\*timeline.data.start\*\/)[^]*?(?=\/\*timeline.data.end\*\/)/, data);
-    fs.writeFileSync(file, newContent);
+    fs.readFile(file, function (err, content) {
+        let data = `\n maps: ${JSON.stringify(map, null, 3)} \n`;
+        let newContent = content.toString().replace(/(?<=\/\*timeline.data.start\*\/)[^]*?(?=\/\*timeline.data.end\*\/)/, data);
+        fs.writeFile(file, newContent, function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
 }
 
 function processPage() {
+    listDirectory(rootDir, writePageData)
     // 生成文件目录
-    generateDirectory(rootDir, writeDirectoryData);
+    generateDirectory();
     // 生成时间线
-    generateTimeline(timelineFilePath, timelineObjs);
+    generateTimeline(timelineFilePath);
     return true;
 }
 
