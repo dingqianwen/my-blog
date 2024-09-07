be0dfeb7205bad99d9bf6dec60019e6a5ebd2ba9
devin.ding <761945125@qq.com>
2024-09-08 03:40:37 +0800

    兼容移动端歌词切换

--- docs/.vuepress/components/MusicPlayer.vue
+++ docs/.vuepress/components/MusicPlayer.vue
@@ -5,10 +5,10 @@
       <Loading/>
     </div>
 
-    <div class="music_player">
+    <div class="music_player" :style="(!isLargeScreen) ?' display: block':'display: flex'">
 
       <div class="left" v-show="!isLoading && (isLargeScreen || switchPanel)"
-           :style="isLargeScreen?'padding: 80px 120px 0 15px;':'padding: 80px 0px 0 0px;margin: 0 auto;'">
+           :style="isLargeScreen?'padding: 80px 120px 0 15px;':'padding: 80px 0px 0 0px;margin: 0 auto;align-items: center;'">
         <img class="point" src="./assets/img/point.png" alt="">
         <img :class="['bar', playing ? 'play': '']" src="./assets/img/bar.png" alt="">
         <div class="img-outer-container">
@@ -18,9 +18,97 @@
             </div>
           </div>
         </div>
+        <div v-show="isLargeScreen">
+          <div class="progress-bar-wrap">
+            <div class="time">{{ formatTime(currentTime) }} / {{ formatTime(totalTime) }}</div>
+            <div @click="(e)=>progressClick(e)"
+                 style="cursor: pointer;">
+              <div class="progress-bar" :style="{width: progress}"></div>
+            </div>
+          </div>
+          <div class="control">
+            <div :class="['btn', playing ? 'play' : 'pause']" @click.native="onClickSong" title="播放/暂停">
+              <svg v-show="!playing" class="svg" viewBox="0 0 900 1024"
+                   xmlns="http://www.w3.org/2000/svg" p-id="801" width="20" height="20">
+                <path
+                    d="M780.8 475.733333L285.866667 168.533333c-27.733333-17.066667-64 4.266667-64 36.266667v614.4c0 32 36.266667 53.333333 64 36.266667l492.8-307.2c29.866667-14.933333 29.866667-57.6 2.133333-72.533334z"
+                    p-id="802" fill="#ffffff"></path>
+              </svg>
+              <svg v-show="playing" class="icon" viewBox="0 0 1024 1024"
+                   xmlns="http://www.w3.org/2000/svg" p-id="1138" width="20" height="20">
+                <path
+                    d="M349.866667 149.333333h-14.933334c-21.333333 0-36.266667 14.933333-36.266666 34.133334v654.933333c0 19.2 14.933333 34.133333 34.133333 34.133333h14.933333c19.2 0 34.133333-14.933333 34.133334-34.133333V183.466667c2.133333-19.2-12.8-34.133333-32-34.133334z m341.333333 0h-14.933333c-21.333333 0-36.266667 14.933333-36.266667 34.133334v654.933333c0 19.2 14.933333 34.133333 34.133333 34.133333h14.933334c19.2 0 34.133333-14.933333 34.133333-34.133333V183.466667c2.133333-19.2-12.8-34.133333-32-34.133334z"
+                    p-id="1139" fill="#ffffff"></path>
+              </svg>
+            </div>
+            <div class="btn share" title="分享" @click.native="copyHandle">
+              <svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"
+                   p-id="2826" width="15" height="15">
+                <path
+                    d="M512 42.666667a37.12 37.12 0 1 1 0 74.026666H227.84a111.573333 111.573333 0 0 0-111.146667 111.146667v568.32a111.573333 111.573333 0 0 0 111.146667 111.146667h568.32a111.573333 111.573333 0 0 0 111.146667-111.146667V512A37.12 37.12 0 1 1 981.333333 512v284.16A185.6 185.6 0 0 1 796.16 981.333333H227.84A185.6 185.6 0 0 1 42.666667 796.16V227.84A185.6 185.6 0 0 1 227.84 42.666667z m432.213333 0A37.12 37.12 0 0 1 981.333333 79.786667V277.333333a37.12 37.12 0 1 1-74.026666 0V169.173333L525.866667 550.613333a37.12 37.12 0 0 1-52.48 0 37.12 37.12 0 0 1 0-52.48L854.826667 116.693333H746.666667A37.12 37.12 0 1 1 746.666667 42.666667z"
+                    fill="#ffffff" p-id="2827"></path>
+                <path
+                    d="M796.16 986.666667H227.84a192 192 0 0 1-190.506667-190.506667V227.84a192 192 0 0 1 190.506667-190.506667H512a42.666667 42.666667 0 1 1 0 85.333334H227.84a106.666667 106.666667 0 0 0-106.666667 106.666666v566.826667a106.666667 106.666667 0 0 0 106.666667 106.666667h568.32a106.666667 106.666667 0 0 0 106.666667-106.666667V512a42.666667 42.666667 0 1 1 85.333333 0v284.16a192 192 0 0 1-192 190.506667z m-568.32-938.666667a180.48 180.48 0 0 0-179.84 179.84v568.32a180.48 180.48 0 0 0 179.84 179.84h568.32a180.48 180.48 0 0 0 179.84-179.84V512a31.786667 31.786667 0 1 0-64 0v284.16a117.12 117.12 0 0 1-116.48 116.48H227.84a117.12 117.12 0 0 1-116.48-116.48V227.84a117.12 117.12 0 0 1 116.48-116.48H512a31.786667 31.786667 0 1 0 0-64z m271.786667 518.826667a42.666667 42.666667 0 0 1-42.666667-42.666667 42.666667 42.666667 0 0 1 12.373333-29.866667L842.026667 122.026667H746.666667a42.666667 42.666667 0 1 1 0-85.333334h197.546666a42.666667 42.666667 0 0 1 42.666667 42.666667V277.333333a42.666667 42.666667 0 1 1-85.333333 0V181.973333L529.706667 554.666667a42.666667 42.666667 0 0 1-30.08 12.16zM867.626667 111.36l-390.4 390.613333a31.36 31.36 0 0 0 0 44.8 32.426667 32.426667 0 0 0 44.8 0L912.64 156.16V277.333333a31.786667 31.786667 0 1 0 64 0V79.786667a31.786667 31.786667 0 0 0-31.786667-31.786667H746.666667a31.786667 31.786667 0 1 0 0 64z"
+                    fill="#ffffff" p-id="2828"></path>
+              </svg>
+            </div>
+            <div class="btn switch" @click="doSwitchPanel" title="切换面板" v-if="!isLargeScreen">
+              <!-- <svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" p-id="5847" width="20" height="20"><path d="M883.8 407.9H726.7v249.3c0 58.3-47.2 105.5-105.5 105.5h-179v86.8c0.1 40.2 32.6 72.7 72.8 72.8h368.9c40.2-0.1 72.7-32.6 72.8-72.8V480.7c0-19.3-7.6-37.9-21.3-51.5-13.7-13.7-32.3-21.3-51.6-21.3z m0 0" fill="#ffffff" p-id="5848" data-spm-anchor-id="a313x.7781069.0.i13" class="selected"></path><path d="M515 342.3h211.8V208.5c0-58.3-47.2-105.5-105.5-105.5H172.5C114.2 103 67 150.2 67 208.5v448.6c0 58.3 47.2 105.5 105.5 105.5h204.1V480.7c0.1-76.4 62-138.3 138.4-138.4z m0 0" fill="#ffffff" p-id="5849" data-spm-anchor-id="a313x.7781069.0.i14" class="selected"></path><path d="M726.7 657.2V407.9H515c-40.2 0.1-72.7 32.6-72.8 72.8v282h179c28 0 54.8-11.1 74.6-30.9 19.8-19.8 30.9-46.6 30.9-74.6z m0 0" fill="#ffffff" p-id="5850" data-spm-anchor-id="a313x.7781069.0.i15" class="selected"></path></svg> -->
+              <svg class="icon" viewBox="0 0 1137 1024" xmlns="http://www.w3.org/2000/svg"
+                   p-id="57234" width="20" height="20">
+                <path
+                    d="M775.509333 843.320889V364.032a119.751111 119.751111 0 0 0-119.808-119.751111H299.406222V119.751111C299.406222 53.646222 353.052444 0 419.157333 0h598.869334C1084.131556 0 1137.777778 53.646222 1137.777778 119.751111v603.761778a119.751111 119.751111 0 0 1-119.751111 119.808h-242.517334z"
+                    p-id="57235" fill="#ffffff"></path>
+                <path
+                    d="M0 301.169778m119.751111 0l479.118222 0q119.751111 0 119.751111 119.751111l0 483.328q0 119.751111-119.751111 119.751111l-479.118222 0q-119.751111 0-119.751111-119.751111l0-483.328q0-119.751111 119.751111-119.751111Z"
+                    p-id="57236" fill="#ffffff"></path>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="right" v-if="!isLoading && (isLargeScreen || !switchPanel)">
+        <div class="lyric-container" style="transition: all .2s ease-in-out;opacity: 1">
+          <div class="music-name">
+            <p>{{ title }}</p>
+            <p>歌手：{{ signer }}&emsp;&emsp;专辑：{{ albumName }}</p>
+          </div>
+          <p v-if="nolyric" class="noLyric">还没有歌词哦~</p>
+          <Scroller
+              :data="lyric"
+              :options="{disableTouch: false}"
+              @init="onInitScroller"
+              class="lyric-wrap"
+              ref="scroller"
+              v-else
+          >
+            <div>
+              <div
+                  :class="getActiveCls(index)"
+                  :key="index"
+                  class="lyric-item"
+                  ref="lyric"
+                  v-for="(l,index) in lyricWithTranslation"
+              >
+                <p
+                    :key="contentIndex"
+                    class="lyric-text"
+                    v-for="(content, contentIndex) in l.contents"
+                >{{ content }}</p>
+              </div>
+            </div>
+          </Scroller>
+        </div>
+      </div>
+
+      <!--适配移动端，控制器从左侧移动到下面显示-->
+      <div class="pcl" v-show="!isLoading && (!isLargeScreen)"
+           style="padding: 10px 0 0 0; margin: 0 auto;">
         <div class="progress-bar-wrap">
           <div class="time">{{ formatTime(currentTime) }} / {{ formatTime(totalTime) }}</div>
-          <div  @click="progressClick" @mousedown="progressStartDrag" ref="progressBarContainer" style="cursor: pointer;">
+          <div @click="(e)=>progressClick(e)"
+               style="cursor: pointer;">
             <div class="progress-bar" :style="{width: progress}"></div>
           </div>
         </div>
@@ -62,40 +150,6 @@
                   p-id="57236" fill="#ffffff"></path>
             </svg>
           </div>
-        </div>
-      </div>
-
-      <div class="right" v-if="!isLoading && (isLargeScreen || !switchPanel)">
-        <div class="lyric-container" style="transition: all .2s ease-in-out;opacity: 1">
-          <div class="music-name" @click="doSwitchPanel">
-            <p>{{ title }}</p>
-            <p>歌手：{{ signer }}&emsp;&emsp;专辑：{{ albumName }}</p>
-          </div>
-          <p v-if="nolyric" class="noLyric">还没有歌词哦~</p>
-          <Scroller
-              :data="lyric"
-              :options="{disableTouch: false}"
-              @init="onInitScroller"
-              class="lyric-wrap"
-              ref="scroller"
-              v-else
-          >
-            <div>
-              <div
-                  :class="getActiveCls(index)"
-                  :key="index"
-                  class="lyric-item"
-                  ref="lyric"
-                  v-for="(l,index) in lyricWithTranslation"
-              >
-                <p
-                    :key="contentIndex"
-                    class="lyric-text"
-                    v-for="(content, contentIndex) in l.contents"
-                >{{ content }}</p>
-              </div>
-            </div>
-          </Scroller>
         </div>
       </div>
 
@@ -285,37 +339,12 @@ export default {
     }
   },
   methods: {
-    progressStartDrag(event) {
-      // 暂停播放
-      if(this.playing){
-        // 如果当前在播放中，标记拖摘完后继续播放
-        this.audio.pause()
-      }
-      this.isProgressDragging = true;
-      this.updateProgress(event);
-      document.addEventListener('mousemove', this.progressOnMouseMove);
-      document.addEventListener('mouseup', this.progressStopDrag);
-    },
-    progressOnMouseMove(event) {
-      if (this.isProgressDragging) {
-        this.updateProgress(event);
-      }
-    },
-    progressStopDrag() {
-      this.isProgressDragging = false;
-      document.removeEventListener('mousemove', this.progressOnMouseMove);
-      document.removeEventListener('mouseup', this.progressStopDrag);
-      if(this.playing){
-        // 继续播放
-        this.audio.play()
-      }
-    },
     progressClick(event) {
       this.updateProgress(event);
     },
     updateProgress(event) {
       // 获取进度条容器的宽度和点击位置
-      const containerRect = this.$refs.progressBarContainer.getBoundingClientRect();
+      const containerRect = event.currentTarget.getBoundingClientRect();
       const clickX = event.clientX - containerRect.left;
       const containerWidth = containerRect.width;
       // 计算百分比
@@ -460,7 +489,7 @@ export default {
   font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, "\5FAE\8F6F\96C5\9ED1", Arial, sans-serif !important;
   position: relative;
 
-  .left {
+  .left, .pcl {
     display: flex;
     flex-direction: column;
     position: relative;
